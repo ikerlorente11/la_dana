8917f63db01b97c20c44de2b94ecdc573d884f65
ikerlorente11 <ikerlorentecalvo@gmail.com>
2024-11-09 23:39:51 +0100

    Style fixes

--- app/info.php
+++ app/info.php
@@ -2,7 +2,7 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
     <title>La Dana Relatos</title>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
