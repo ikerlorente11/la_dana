d30ffa24794bc9d77a05c074ce6d1dddbbf65ff5
ikerlorente11 <ikerlorentecalvo@gmail.com>
2024-11-15 09:49:16 +0100

    Better seo

--- app/new.php
+++ app/new.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
     <script src="js/new.js" defer></script>
     <link rel="stylesheet" href="styles/style.css">
+    <link rel="canonical" href="https://ladanarelatos.ct.ws/">
 
     <link rel="icon" type="image/x-icon" href="/assets/favicon.png">
 </head>
